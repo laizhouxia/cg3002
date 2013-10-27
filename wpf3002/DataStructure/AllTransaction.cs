5c7042a47df99fd439ffd05ab4664bb95b0a9b34
laizhouxia <znj472982642@gmail.com>
2013-10-27 11:34:33 +0800

    can load transaction

---
+++ wpf3002/DataStructure/AllTransaction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpf3002.DataStructure
+{
+    public class AllTransaction
+    {
+        public String barcode { get; set; }
+        public String quantity { get; set; }
+        public String price { get; set; }
+        public DateTime date { get; set; }
+        public AllTransaction(String _barcode, String _quantity, String _price, DateTime _date)
+        {
+            barcode = _barcode;
+            quantity = _quantity;
+            price = _price;
+            date = _date;
+        }
+    }
+}
--- wpf3002/MainWindow.xaml.cs
+++ wpf3002/MainWindow.xaml.cs
@@ -611,5 +611,39 @@ namespace wpf3002
             }
         }
 
+        private void loadTransaction_Click(object sender, RoutedEventArgs e)
+        {
+            // Create OpenFileDialog
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+            // Set filter for file extension and default file extension
+            dlg.DefaultExt = ".txt";
+            dlg.Filter = "Text documents (.txt)|*.txt";
+
+            // Display OpenFileDialog by calling ShowDialog method
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Get the selected file name and display in a TextBox
+            if (result == true)
+            {
+                // Open document
+                String filename = dlg.FileName;
+                DataStructure.Transaction tempTransaction = new DataStructure.Transaction();
+                foreach (String line in File.ReadAllLines(filename))
+                {
+
+                    String[] tokens = line.Split(':');
+                    String _barcode = tokens[0];
+                    String _quantity = tokens[1];
+                    String _price = tokens[2];
+                    String[] _dates = tokens[3].Split('/');
+                    DateTime _date = new DateTime(Convert.ToInt32(_dates[2]), Convert.ToInt32(_dates[1]), Convert.ToInt32(_dates[0]));
+                    tempTransaction.add(_barcode, _quantity, _price);
+                    tempTransaction.date = _date;
+                }
+                wholeDayTransaction.Add(tempTransaction);
+            }
+        }
+
     }
 }
