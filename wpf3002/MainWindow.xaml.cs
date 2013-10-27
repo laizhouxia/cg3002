77b429a40390539788ee8d8fe3c18c1e495bb3c4
laizhouxia <znj472982642@gmail.com>
2013-10-27 17:46:11 +0800

    bug fixed

--- wpf3002/MainWindow.xaml.cs
+++ wpf3002/MainWindow.xaml.cs
@@ -597,7 +597,10 @@ namespace wpf3002
                 for (int i = 0; i < tempTransaction.items.Count; i++)
                     for (int j = 0; j < data.Count; j++)
                         if (data[j].barcode == tempTransaction.items[i].barcode)
+                        {
                             _oneTransactionView.Add(new DataStructure.TransactionViewItem(data[j].name, data[j].barcode, data[j].daily_price, tempTransaction.items[i].quantity));
+                            break;
+                        }
             }
         }
         string[] a = new string[] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
